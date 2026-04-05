2c3957d5055dc4e0f4f15a3fcc6a2d63db6a6be7
Nivesh Jain <niveshjain42@gmail.com>
2026-04-05 18:44:14 +0530

    chore: fixed the bug of header scrolling

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -25,7 +25,7 @@ export const Header = ({ siteName, navLinks }: HeaderProps) => {
 
 
     return (
-        <header className="fixed top-0 left-0 right-0 z-50 bg-background border-b border-border">
+        <header className="fixed top-0 left-0 right-0 z-50 bg-background border-b border-border will-change-transform">
             <div className="flex items-center justify-between px-6 py-4">
                 <Link
                     href="/"
